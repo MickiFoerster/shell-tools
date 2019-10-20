3f2f970978aef6bebf55ce5dfee4391c6cccf7a2
micki <github@com-science.de>
2019-10-20 19:08:07 +0200

    add start yocto script

---
+++ start-yocto.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -e
+
+yoctobranch=thud
+directory=$HOME/yocto
+
+func getsources() {
+  remoteurl=$1
+  dir=$2
+  if [ ! -d ${dir} ]; then
+    git clone -b ${yoctobranch} ${remoteurl} $dir
+  else
+    cd ${dir}
+    git pull
+    cd - 
+  fi
+}
+
+if [ ! -e "${directory}" ]; then mkdir -p "${directory}"; fi
+cd "${directory}"
+
+getsources git://git.yoctoproject.org/poky.git sources/poky 
+getsources https://github.com/agherzan/meta-raspberrypi.git sources/meta-raspberrypi
+getsources https://github.com/openembedded/meta-openembedded.git sources/meta-openembedded
+
+echo "poky, meta-raspberrypi, and meta-openembedded downloaded. Now, type \". ./poky/oe-init-build-env\" to create/enter the build folder"
