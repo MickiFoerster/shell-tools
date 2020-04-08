c7adb80fab6bee61f6df3a728c9687124c260420
micki <github@com-science.de>
2020-04-08 22:30:55 +0200

    adjust install script for kubernetes

--- install-kubernetes.sh
+++ install-kubernetes.sh
@@ -9,3 +9,13 @@ sudo usermod -a -G microk8s $USER
 echo "alias kubectl='microk8s.kubectl'" >> ~/.bashrc
 EOF
 
+
+# The following installs a docker container that gives you a shell into the
+# cluster
+kubectl apply -f https://k8smastery.com/shpod.yaml
+kubectl attach --namespace=shpod -ti shpod
+
+cat <<EOF
+# After you finished the course or if you want to remove shpod use
+kubectl delete -f https://k8smasery.com/shpod.yaml
+EOF
