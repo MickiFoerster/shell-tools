c0f527708027ea88fdf76c4dd507f5bf8ba05c6c
micki <github@com-science.de>
2020-11-15 16:31:07 +0100

    Redirect stderr to null

--- ctags_with_deps.sh
+++ ctags_with_deps.sh
@@ -2,5 +2,5 @@
 
 # Found here: https://vim.fandom.com/wiki/Generate_ctags_file_for_a_C/C%2B%2B_source_file_with_all_of_their_dependencies_(standard_headers,_etc)
 
-gcc -M $* | sed -e 's/[\\ ]/\n/g' | \
+gcc -M $* 2>/dev/null | sed -e 's/[\\ ]/\n/g' | \
     sed -e '/^$/d' -e '/\.o:[ \t]*$/d' | ctags -L - --c++-kinds=+p --fields=+iaS --extra=+q
