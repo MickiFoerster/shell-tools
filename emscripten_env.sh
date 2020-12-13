f8e34f39c9c7e50d564a3f49cc53f5884c6a9a43
micki <github@com-science.de>
2020-12-13 10:57:06 +0100

    Fix makefile syntax of shell variable

--- emscripten_env.sh
+++ emscripten_env.sh
@@ -1,15 +1,15 @@
 #!/bin/sh	
-dir=$$HOME/workspace/external-projects && \
-cd $$dir && \
+dir=$HOME/workspace/external-projects && \
+cd $dir && \
 dir=emsdk && \
-if [ -d $$dir ]; then 
-	cd $$dir; 
+if [ -d $dir ]; then 
+	cd $dir; 
 	git pull;
 else
 	git clone https://github.com/emscripten-core/emsdk.git;
-	cd $$dir ; 
+	cd $dir ; 
 fi && \
 dir=`pwd` && \
-$$dir/emsdk install latest && \
-$$dir/emsdk activate lastest && \
-source $$dir/emsdk_env.sh
+$dir/emsdk install latest && \
+$dir/emsdk activate lastest && \
+source $dir/emsdk_env.sh
