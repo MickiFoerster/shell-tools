82a7d48d06e2929a5287b4f74e1edb1ceee7640c
micki <github@com-science.de>
2021-11-18 16:51:24 +0100

    Add script that enable cgroup V2 usage for systemD

---
+++ cgroup-patcher.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+ 
+if ! grep -q cgroup2 /proc/filesystems; then
+    echo "error: cgroupv2 is not supported"
+    exit 1
+else
+    echo "cgroup V2 found"
+fi
+
+kernelarg='systemd.unified_cgroup_hierarchy=1'
+f=/etc/default/grub
+if [ ! -f $f ]; then
+    echo "error: cannot find grub default file"
+    exit 1
+fi
+
+cmd=awk
+if ! command -v ${cmd} >/dev/null ; then
+    echo "error: command ${cmd} not found"
+    exit 1
+fi
+
+set -e
+
+tmpfile=/tmp/awk-repl-$RANDOM
+awk -F= -v karg=${kernelarg} '
+/GRUB_CMDLINE_LINUX=/          { r = gensub(/"(.*)"/,  "\\1 " karg, "g", $2); print $1"=\""r"\""; next; }
+                               { print ; }
+' $f  >${tmpfile}
+
+chmod 644 ${tmpfile}
+mv ${tmpfile} $f
+rm -f ${tmpfile}
+
+set +e
