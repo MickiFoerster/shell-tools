97c47458b714628a13243de43aabd449d576c520
micki <github@com-science.de>
2022-01-09 11:49:08 +0100

    Add note about install shell completion

--- install-kubebuilder.sh
+++ install-kubebuilder.sh
@@ -1,3 +1,10 @@
 # download kubebuilder and install locally.
 curl -L -o kubebuilder https://go.kubebuilder.io/dl/latest/linux/amd64
 chmod +x kubebuilder && mv kubebuilder $HOME/bin/
+
+cat << EOM
+Enable shell completion by adding
+
+complete -C kubebuilder completion <bash|zsh>
+EOM
+
