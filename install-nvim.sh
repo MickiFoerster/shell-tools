e127ebaf6a1915a9ba0904f9d15dcac82b4e4b4b
micki <github@com-science.de>
2022-06-12 21:03:43 +0200

    Script for install nvim latest version

---
+++ install-nvim.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+set -e
+oldwd=`pwd`
+cd /tmp
+nvim_version=$(curl -L https://github.com/neovim/neovim/releases/latest | \
+    grep -E "href=\"[^\"]*\/[^\"]*\/nvim-linux64.tar.gz\"" | \
+    sed "s#.*/download/\(v[0-9\.]*\)/nvim-linux64.tar.gz.*#\1#")
+echo Download nvim in version ${nvim_version}
+curl -LO "https://github.com/neovim/neovim/releases/download/${nvim_version}/nvim-linux64.tar.gz"
+mkdir -p $HOME/programs
+cd $HOME/programs
+tar xf /tmp/nvim-linux64.tar.gz
+cd nvim-linux64/bin
+echo "Installed nvim ${nvim_version} into $PWD."
+a=$PWD/nvim
+b=$(which nvim)
+if [ "$a" != "$b" ] ; then
+    echo "warning: PATH is NOT pointing to the installed nvim binary"
+    echo "Remember to add this path to PATH"
+fi
+cd ${oldwd}
