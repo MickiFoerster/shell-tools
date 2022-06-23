7611dc6f0a38960ddb6c1971469610b1f327b95b
micki <github@com-science.de>
2022-06-23 15:28:45 +0200

    Rust installation script

---
+++ install-rust.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -ex
+
+# rustup
+curl --proto '=https' --tlsv1.3 -sSf https://sh.rustup.rs | sh
+
+# cargo packages
+for pkg in \
+    cargo-edit    \
+    cargo-expand    \
+    cargo-watch    \
+    grcov    \
+    rustlings    \
+    sea-orm-cli    \
+    sqlx-cli    \
+    tokio-console    \
+; do
+    cargo install ${pkg}
+done
+
+
+
+
