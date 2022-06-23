d99ac1914e46d4fce74bc8ebe85dd5250f097941
micki <github@com-science.de>
2022-06-23 08:34:46 +0200

    Make installation of nvim easier

--- install-nvim.sh
+++ install-nvim.sh
@@ -20,3 +20,10 @@ if [ "$a" != "$b" ] ; then
     echo "Remember to add this path to PATH"
 fi
 cd ${oldwd}
+
+
+# Install vim-plub (https://github.com/junegunn/vim-plug)
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+nvim +PlugInstall!
+#nvim +PlugUpdate!
