969e7ca5a198b21416effadb217b30ce19c73fbd
micki <github@com-science.de>
2022-08-07 08:20:34 +0200

    install rust script

--- install-rust.sh
+++ install-rust.sh
@@ -5,6 +5,10 @@ set -ex
 # rustup
 curl --proto '=https' --tlsv1.3 -sSf https://sh.rustup.rs | sh
 
+# In order to use `cargo +nightly expand`
+rustup toolchain install nightly --allow-downgrade
+
+
 # cargo packages
 for pkg in \
     cargo-edit    \
