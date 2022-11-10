ad42ca63183079434110f2c43f93d1a4c9d22891
micki <github@com-science.de>
2022-11-10 22:08:58 +0100

    install script for helm

---
+++ install-helm.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -ex
+
+cd /tmp
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+cat get_helm.sh | \
+	sed "s#/usr/local/bin#$HOME/bin#" | \
+	sed "s#runAsRoot cp##" | \
+	> get_helm.sh.tmp
+mv get_helm.sh.tmp get_helm.sh
+chmod 700 get_helm.sh
+./get_helm.sh
+
+set +ex
