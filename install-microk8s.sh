3012a03da2baef64b44873d8541ec254703dc994
micki <github@com-science.de>
2022-11-10 14:30:57 +0100

    install script for microk8s

--- install-microk8s.sh
+++ install-microk8s.sh
@@ -4,10 +4,12 @@ set -ex
 
 sudo snap install microk8s --classic --channel=1.25
 sudo usermod -a -G microk8s $USER
+#newgrp microk8s
 sudo chown -f -R $USER ~/.kube
 microk8s status --wait-ready
 alias k='microk8s kubectl'
-microk8s enable dns storage
+
+microk8s enable dashboard dns registry istio storage
 
 # microk8s stop
 echo "Stop microk8s with 'microk8s stop'"
