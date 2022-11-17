b5be1d5615e7ee0873288515ebbf4fb385d74196
micki <github@com-science.de>
2022-11-17 22:12:28 +0100

    install docker script

--- install-docker.sh
+++ install-docker.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 
-set -e
+set -ex
 
-sudo snap install docker
+#sudo snap install docker
+sudo apt-get remove docker docker-engine docker.io containerd runc
+sudo apt-get update
+sudo apt-get install     ca-certificates     curl     gnupg     lsb-release
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+which docker
+docker version
+docker run hello-world
+
 
 # docker-compose
 curl -L "https://github.com/docker/compose/releases/download/1.28.5/docker-compose-$(uname -s)-$(uname -m)"  > docker-compose.bin
@@ -28,3 +41,6 @@ pass init "$(git config --global user.name)"
 
 # Set configuration to use encrypted password
 sed -i '0,/{/s/{/{\n\t"credsStore": "pass",/' ~/.docker/config.json
+
+set +ex
+
