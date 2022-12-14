5bfc9960925796806d997d808e8f6e04801fc2b4
micki <github@com-science.de>
2022-12-14 11:40:01 +0100

    update rust tooling

--- update-rust-analyzer.sh
+++ update-rust-analyzer.sh
@@ -4,6 +4,7 @@ url=$(curl -s -L "https://github.com/rust-lang/rust-analyzer/releases/latest"|\
     egrep "href=\"/.*rust-analyzer-x86_64-unknown-linux-gnu.gz\"" | \
     sed "s#.*href=\"\(/.*rust-analyzer-x86_64-unknown-linux-gnu.gz\)\".*#\1#" |\
     xargs -iarg echo https://github.comarg)
+url=https://github.com/rust-lang/rust-analyzer/releases/download/2022-12-12/rust-analyzer-x86_64-unknown-linux-gnu.gz
 set -e
 cd /tmp/
 rm -f rust-analyzer-x86_64-unknown-linux-gnu*
@@ -13,6 +14,6 @@ if [ -f $HOME/.cargo/bin/rust-analyzer ]; then
     mv $HOME/.cargo/bin/rust-analyzer $HOME/.cargo/bin/rust-analyzer.old
 fi
 chmod 700 rust-analyzer-x86_64-unknown-linux-gnu
-mv rust-analyzer-x86_64-unknown-linux-gnu $HOME/.cargo/bin/rust-analyzer
+mv -v rust-analyzer-x86_64-unknown-linux-gnu $HOME/.cargo/bin/rust-analyzer
 echo "rust-analyzer successfully updated"
 set +e
