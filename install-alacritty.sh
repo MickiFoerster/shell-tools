356d19f5e3a88ed3762a694911a241564ff4f4ae
micki <github@com-science.de>
2023-01-05 16:54:02 +0100

    install alacritty

--- install-alacritty.sh
+++ install-alacritty.sh
@@ -3,6 +3,13 @@
 set -ex
 
 cd /tmp
+
+rustup override set stable
+rustup update stable
+
+sudo apt-get install cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3
+
+rm -rf /tmp/alacritty
 git clone https://github.com/alacritty/alacritty.git
 cd alacritty
 cargo install alacritty
