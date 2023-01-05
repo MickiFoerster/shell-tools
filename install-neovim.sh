3153db7ce9c9ad8d892c87e61bc66ef839fe0951
micki <github@com-science.de>
2023-01-05 21:58:08 +0100

    Install packer in neovim

--- install-neovim.sh
+++ install-neovim.sh
@@ -155,7 +155,6 @@ if [[ -d $dir ]] ; then
 	cd -
 fi
 
-
-#git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
 set +ex 
