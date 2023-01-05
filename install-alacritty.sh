842b2ad89d1214cf4c94969f36746efa7a5bda7f
micki <github@com-science.de>
2023-01-05 17:07:00 +0100

    update alacritty

--- install-alacritty.sh
+++ install-alacritty.sh
@@ -4,8 +4,8 @@ set -ex
 
 cd /tmp
 
-rustup override set stable
-rustup update stable
+#rustup override set stable
+#rustup update stable
 
 sudo apt-get install cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3
 
