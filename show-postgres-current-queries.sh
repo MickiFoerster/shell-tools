2de06bea1a46c3b2b97f42376f677660f82b0d9b
micki <github@com-science.de>
2023-01-20 18:00:00 +0100

    Add postgres monitor script

---
+++ show-postgres-current-queries.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+cat <<EOM | psql
+SELECT
+	pid,
+	age(clock_timestamp(), query_start),
+	usename as username,
+	query
+FROM pg_stat_activity
+WHERE
+	query != '<IDLE>'
+	AND query NOT ILIKE '%pg_stat_activity%'
+ORDER BY query_start desc;
+
+EOM
