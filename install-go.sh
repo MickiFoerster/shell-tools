bc96f1e60393df098accb2503c8b67a58787e37d
micki <github@com-science.de>
2023-02-18 10:29:10 +0100

    go install script

--- install-go.sh
+++ install-go.sh
@@ -6,7 +6,7 @@ set -ex
 cd /tmp
 curl -LO https://go.dev/dl/go${VERSION}.linux-amd64.tar.gz
 mkdir -p $HOME/programs
-rm -rf $HOME/programs/go.old
+rm -rf $HOME/programs/go
 cd $HOME/programs
 tar xvf /tmp/go${VERSION}.linux-amd64.tar.gz
 echo "go version ${VERSION} installed successfully"
