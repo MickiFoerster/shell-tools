5c1296a58d16bf4ca5176ae6158d2cb0aed84b98
micki <github@com-science.de>
2023-02-19 11:33:05 +0100

    navicat

--- install-navicat.sh
+++ install-navicat.sh
@@ -2,9 +2,8 @@
 
 set -ex
 cd /tmp/
-curl -LO 'https://www.navicat.com/download/direct-download?product=navicat16-pgsql-en.AppImage&location=1'
-curl -LO 'https://www.navicat.com/download/direct-download?product=modeler3-en.AppImage&location=1'
-
+curl -LO 'https://dn.navicat.com/download/navicat16-pgsql-en.AppImage'
+curl -LO 'https://dn.navicat.com/download/modeler3-en.AppImage'
 sudo apt install libfuse2
 chmod 755 modeler3-en.AppImage
 chmod 755 navicat16-pgsql-en.AppImage
