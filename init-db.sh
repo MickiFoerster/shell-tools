fc2d6d716f9d1886ec21bdc9b57bdb12da7cd4b0
micki <github@com-science.de>
2023-02-22 18:41:15 +0100

    Case where psql is not installed

--- init-db.sh
+++ init-db.sh
@@ -45,21 +45,26 @@ gnome-terminal -- docker run \
 postgres:${DOCKER_TAG} \
 postgres -N 2048 # ^ Increased maximum number of connections for testing purposes
 
-# Keep pinging Postgres until it's ready to accept commands
-export PGPASSWORD="${DB_PASSWORD}"
-counter=1
-until psql -h "localhost" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c '\q' 2>/dev/null; do
-    sleep 0.1
-    let counter=${counter}+1
-    if [[ ${counter} -gt 10 ]]; then
-        >&2 echo "Postgres is still unavailable - sleeping"
-    fi
-    if [[ ${counter} -gt 20 ]]; then
-        echo "could not start docker container"
-        exit 1
-    fi
-done
->&2 echo "Postgres is up and running on port ${DB_PORT}!"
+if command -v psql >/dev/null; then
+	# Keep pinging Postgres until it's ready to accept commands
+	export PGPASSWORD="${DB_PASSWORD}"
+	counter=1
+	until psql -h "localhost" -U "${DB_USER}" -p "${DB_PORT}" --dbname "${DB_NAME}" -c '\q' 2>/dev/null; do
+	    sleep 0.1
+	    let counter=${counter}+1
+	    if [[ ${counter} -gt 10 ]]; then
+		>&2 echo "Postgres is still unavailable - sleeping"
+	    fi
+	    if [[ ${counter} -gt 20 ]]; then
+		echo "could not start docker container"
+		exit 1
+	    fi
+	done
+	>&2 echo "Postgres is up and running on port ${DB_PORT}!"
+else
+	echo "Waiting for postgres container to come up ..."
+	sleep 3
+fi
 
 export DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@localhost:${DB_PORT}/${DB_NAME}
 echo "Remember so set the following env variable:"
