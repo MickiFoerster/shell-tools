c623f463e5d30b250d5999540a6165419345a58c
micki <github@com-science.de>
2023-08-05 16:43:36 +0200

    install scrapy

--- install-scrapy.sh
+++ install-scrapy.sh
@@ -11,3 +11,15 @@ source ./python-venv-scrapy/bin/activate
 # autopep8: autopep8 automatically formats Python code to conform to the `PEP 8`_ style.
 python -m pip install scrapy pylint autopep8
 
+echo "For automatic loading the venv you can use for example:"
+cat <<EOM
+
+function scrapy() {
+    if ! echo \$PS1 | grep -q python-venv-scrapy; then
+        source \$HOME/python-venv-scrapy/bin/activate
+    fi
+    unset -f scrapy
+    scrapy \$@
+}
+
+EOM
