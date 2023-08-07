e921877fa00de45945ecbe458048cb720e59a612
micki <github@com-science.de>
2023-08-07 10:06:39 +0200

    docker compose version 2.20.2

--- install-docker-compose.sh
+++ install-docker-compose.sh
@@ -2,10 +2,11 @@
 
 set -ex
 
+cd /tmp || exit 1
 # docker-compose
-curl -L "https://github.com/docker/compose/releases/download/v2.16.0/docker-compose-linux-x86_64"  > docker-compose.bin
-chmod +x docker-compose.bin
-mv docker-compose.bin ~/bin/docker-compose
+curl -LO "https://github.com/docker/compose/releases/download/v2.20.2/docker-compose-linux-x86_64"
+mv docker-compose-linux-x86_64 docker-compose
+install docker-compose ~/bin/
 
 set +ex
 
