52c7b6dd5c1df05b57b8075da5d00f7640af907e
micki <github@com-science.de>
2023-08-09 06:50:02 +0200

    update docker installation

--- install-docker-compose.sh
+++ install-docker-compose.sh
@@ -2,11 +2,14 @@
 
 set -ex
 
-cd /tmp || exit 1
-# docker-compose
-curl -LO "https://github.com/docker/compose/releases/download/v2.20.2/docker-compose-linux-x86_64"
+version=v2.20.2
+
+cd /tmp/
+
+curl -LO "https://github.com/docker/compose/releases/download/${version}/docker-compose-linux-x86_64"
 mv docker-compose-linux-x86_64 docker-compose
-install docker-compose ~/bin/
+install docker-compose $HOME/bin/
 
 set +ex
 
+unset version
