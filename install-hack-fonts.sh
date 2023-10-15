e9a0a041aed2c466e30456ce467ac8de533a16db
micki <github@com-science.de>
2023-10-15 10:11:11 +0200

    install nerd hack font

--- install-hack-fonts.sh
+++ install-hack-fonts.sh
@@ -1,5 +1,16 @@
-fc-cache -f -v 
+#!/bin/bash
+
+
+set -ex
+
+cd /tmp/
+rm -f Hack.zip
+curl -LO "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/Hack.zip"
 unzip Hack.zip
+mkdir -p ~/.local/share/fonts/
 mv *.ttf ~/.local/share/fonts/
+
+fc-cache -f -v 
 fc-list | grep Hack
 
+set +ex
