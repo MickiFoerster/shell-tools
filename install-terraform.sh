3770d507198637a5d6192488065445b0f65b7f09
micki <github@com-science.de>
2023-12-16 20:40:31 +0100

    terraform 1.6.6

--- install-terraform.sh
+++ install-terraform.sh
@@ -3,8 +3,9 @@
 set -ex
 
 cd /tmp/
-curl -LO https://github.com/hashicorp/terraform/archive/refs/tags/v1.5.0.tar.gz
-tar xf v1.5.0.tar.gz 
+VERSION=v1.6.6
+curl -LO https://github.com/hashicorp/terraform/archive/refs/tags/${VERSION}.tar.gz
+tar xf ${VERSION}.tar.gz 
 cd terraform-*
 make
 make
