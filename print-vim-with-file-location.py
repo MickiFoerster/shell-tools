bca3627934c0cca53a39d848f277b8cd24c161f6
micki <github@com-science.de>
2024-01-30 14:15:40 +0100

    helper for open files quickly

--- print-vim-with-file-location.py
+++ print-vim-with-file-location.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+import os
 import sys
 
 # no argument given then return
@@ -11,7 +12,21 @@ lst = sys.argv[1].split(":")
 # vim "+call cursor(35,22)"
 # or vim "+normal <LINE>G<COLUMN>|"
 if len(lst) >= 2 and len(lst[1]) > 0:
-    print("{} +{}".format(lst[0], lst[1]), end='')
+    file_path = lst[0]
+    pos = lst[1]
+
+    if not os.path.exists(file_path):
+        lst = file_path.split("/")
+        for i, p in enumerate(lst):
+            pwd = os.getcwd()
+            pwd_lst = pwd.split("/")
+            for j in range(len(pwd_lst)-1, -1, -1):
+                if p == pwd_lst[j]:
+                    rp = "/".join(lst[i+1:])
+                    if os.path.exists(rp):
+                        file_path = rp
+
+    print("{} +{}".format(file_path, pos), end='')
 else:
     for arg in sys.argv[1:]:
         print(f"{arg} ", end='')
