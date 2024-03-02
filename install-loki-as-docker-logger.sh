b8e713fab05c3c3c5f936e558b9e43e39fc4ec41
micki <github@com-science.de>
2024-03-02 17:37:58 +0100

    don't change global docker daemon configuration but rather change docker-compose file

--- install-loki-as-docker-logger.sh
+++ install-loki-as-docker-logger.sh
@@ -2,15 +2,19 @@
 
 sudo docker plugin install grafana/loki-docker-driver:2.9.4 --alias loki --grant-all-permissions
 
-sudo cat <<EOM >>/etc/docker/daemon.json
-{
-    "debug" : true,
-    "log-driver": "loki",
-    "log-opts": {
-        "loki-url": "https://localhost:3100/loki/api/v1/push",
-        "loki-batch-size": "400"
-    }
-}
+echo "Add the following lines to you docker-compose file"
+cat <<EOM 
+services:
+  myservice:
+    image: myservice:1.2.3
+    logging:
+      driver: loki
+      options:
+        loki-url: "http://localhost:3100/loki/api/v1/push"
+        loki-retries: 2
+        loki-max-backoff: 800ms
+        loki-timeout: 1s
+        keep-file: "true"
 
 EOM
 
