4d9398375e4e97d80b7efd2dc0d89d560963cddf
micki <github@com-science.de>
2024-06-22 10:03:11 +0200

    upgrade pip

--- upgrade-python-venvs.sh
+++ upgrade-python-venvs.sh
@@ -1,8 +1,8 @@
 #!/bin/bash  -x
 
-for i in $HOME/python-venv-* ; 
-do
+for i in $HOME/python-venv-*; do
     t=$(echo $i | sed "s#$HOME/python-venv-\(.*\)#\1#")
     source $i/bin/activate
+    pip install --upgrade pip
     pip install --upgrade $t
 done
