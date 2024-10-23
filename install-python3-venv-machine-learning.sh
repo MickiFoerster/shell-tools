6f20a9a1d6c3b0535a924069f89f93d76e5276da
micki <github@com-science.de>
2024-10-23 06:10:36 +0200

    Add cuda

--- install-python3-venv-machine-learning.sh
+++ install-python3-venv-machine-learning.sh
@@ -4,12 +4,12 @@ set -ex
 
 cd $HOME
 
-if ! dpkg -l python3-venv | /usr/bin/grep "^ii"; then 
-	sudo apt install python3-venv
+if ! dpkg -l python3-venv | /usr/bin/grep "^ii"; then
+    sudo apt install python3-venv
 fi
 
 if [[ ! -d machine-learning ]]; then
-	python3 -m venv machine-learning
+    python3 -m venv machine-learning
 fi
 
 . ./machine-learning/bin/activate
@@ -19,7 +19,8 @@ pip install numpy
 pip install matplotlib
 pip install pandas
 pip install -U scikit-learn
-pip install tensorflow
+#pip install tensorflow
+pip install tensorflow[and-cuda]
 pip install keras
 # For CUDA 11.8:
 #pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
