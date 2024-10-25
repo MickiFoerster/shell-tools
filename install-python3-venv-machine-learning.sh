b851c7e9bbeabbeb4a88a2a67c6588f313726a9d
micki <github@com-science.de>
2024-10-25 14:36:30 +0200

    update

--- install-python3-venv-machine-learning.sh
+++ install-python3-venv-machine-learning.sh
@@ -19,11 +19,12 @@ pip install numpy
 pip install matplotlib
 pip install pandas
 pip install -U scikit-learn
-#pip install tensorflow
-pip install tensorflow[and-cuda]
+#pip install tensorflow==2.16.1
+#pip install tensorflow[and-cuda]
+pip install tensorflow[and-cuda]==2.16.1
 pip install keras
 # For CUDA 11.8:
 #pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
-pip3 install torch torchvision torchaudio
+#pip3 install torch torchvision torchaudio
 
 set +ex
