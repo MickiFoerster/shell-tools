ec78e7f2c2e9d0be6e70489b9cb8534ddd601dc3
micki <github@com-science.de>
2024-11-19 13:32:30 +0100

    fix missing quotation

--- gcp-helpers.sh
+++ gcp-helpers.sh
@@ -1,11 +1,11 @@
 function gcp-create_bucket() {
-    if [[ "$1" == "" ]]; then 
+    if [[ "$1" == "" ]]; then
         echo "1st argument is name of the bucket and cannot be empty"
         return 1
     fi
 
     export PROJECT_ID=$(gcloud config get-value project)
-    export BUCKET_NAME="${PROJECT_ID}-$1
+    export BUCKET_NAME="${PROJECT_ID}-$1"
 
     gsutil mb -p ${PROJECT_ID} gs://${BUCKET_NAME}
 }
