dc420ebfb952e93f038dea0d0d1b4cd6436da5f4
micki <github@com-science.de>
2025-01-13 23:18:34 +0100

    better wait times

--- destroy-alibaba-ecs.sh
+++ destroy-alibaba-ecs.sh
@@ -8,18 +8,18 @@ vpc_id=$(cat vpc.id)
 
 # stop instance
 aliyun ecs StopInstance --region ${region} --InstanceId ${instance_id}
-sleep 10
+sleep 30
 # aliyun ecs DescribeInstances --RegionId cn-hongkong  | jq -r .Instances.Instance[].Status
 # must be 'Stopped'
 
 # destroy instance
 aliyun ecs DeleteInstance --region ${region} --InstanceId ${instance_id}
-sleep 3
+sleep 10
 # delete security group
 aliyun ecs DeleteSecurityGroup --region ${region} --SecurityGroupId ${sec_grp_id}
-sleep 3
+sleep 5
 # delete vSwitch
 aliyun vpc DeleteVSwitch --region ${region} --RegionId ${region} --VSwitchId ${vswitch_id}
-sleep 3
+sleep 5
 # delete VPC
 aliyun vpc DeleteVpc --region ${region} --RegionId ${region} --VpcId ${vpc_id}
