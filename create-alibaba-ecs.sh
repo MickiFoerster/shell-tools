6d6c1e3ef8c0564c5ad5d23f7f2aa64331a19631
micki <github@com-science.de>
2025-01-13 23:09:47 +0100

    Add Alibaba ecs create, destroy scripts

---
+++ create-alibaba-ecs.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+# Configure the AccessKey ID and AccessKey secret used by Alibaba Cloud CLI.
+# Note: Make sure that the AccessKey ID and AccessKey secret are securely
+# configured by configuring environment variables or by using a configuration
+# file.
+
+set -e
+
+region=cn-hongkong
+
+# 1. Configure variables.
+INSTANCE_NAME="ecs_cli_demo"
+
+#3. Create a Virtual Private Cloud (VPC), a vSwitch, and a security group.
+echo "Creating a VPC..."
+VpcId=$(aliyun vpc CreateVpc --RegionId ${region} --CidrBlock 192.168.0.0/16 | jq -r .VpcId)
+echo VPC ID is '${VpcId}'
+echo ${VpcId} >vpc.id
+
+printf "Wait status is 'available' ... "
+aliyun vpc DescribeVpcAttribute --RegionId ${region} --VpcId ${VpcId} --waiter expr='Status' to=Available
+echo "done"
+
+echo "Creating a vSwitch..."
+VSwitchId=$(aliyun vpc CreateVSwitch --CidrBlock 192.168.0.0/24 --VpcId ${VpcId} --ZoneId=${region}-d | jq -r .VSwitchId)
+echo ${VSwitchId} >vswitch.id
+
+echo "Creating a security group..."
+SecurityGroupId=$(aliyun ecs CreateSecurityGroup --RegionId ${region} --VpcId ${VpcId} | jq -r .SecurityGroupId)
+echo ${SecurityGroupId} >securitygroup.id
+aliyun ecs AuthorizeSecurityGroup --RegionId ${region} --SecurityGroupId ${SecurityGroupId} --IpProtocol tcp --SourceCidrIp 0.0.0.0/0 --PortRange 22/22 >/dev/null 2>&1
+
+PASSWORD=$(openssl rand -base64 20)
+echo
+echo ${PASSWORD} >ecs-password
+
+# 4. Run the command used to create an ECS instance.
+echo "Creating an ECS instance..."
+INSTANCE_ID_RAW=$(aliyun ecs RunInstances \
+    --RegionId ${region} \
+    --ImageId ubuntu_22_04_x64_20G_alibase_20240508.vhd \
+    --InstanceType ecs.e-c1m1.large \
+    --SecurityGroupId ${SecurityGroupId} \
+    --VSwitchId ${VSwitchId} \
+    --InstanceName ${INSTANCE_NAME} \
+    --InstanceChargeType PostPaid \
+    --InternetMaxBandwidthOut 1 \
+    --Password $PASSWORD \
+    --SystemDisk.Category cloud_essd \
+    --SystemDisk.Size 40)
+
+# 5. Obtain the InstanceId parameter for subsequently returned information.
+INSTANCE_ID=$(echo "$INSTANCE_ID_RAW" | jq -r '.InstanceIdSets.InstanceIdSet[]')
+echo ${INSTANCE_ID} >instance.id
+
+# 6. Wait for 20 seconds for the ECS instance to be created.
+echo "Waiting for the ECS instance to be created..."
+sleep 20
+
+# 7. Query the status of the ECS instance.
+echo "Querying the status of the ECS instance..."
+INSTANCE_ID_QUOTED=$(printf '"%s"' "$INSTANCE_ID")
+aliyun ecs DescribeInstances \
+    --RegionId ${region} \
+    --InstanceIds "[${INSTANCE_ID_QUOTED}]" \
+    --output cols=InstanceId,InstanceName,InstanceType,ImageId,Status rows=Instances.Instance[]
+
+set +e
---
+++ destroy-alibaba-ecs.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+region=cn-hongkong
+instance_id=$(cat instance.id)
+sec_grp_id=$(cat securitygroup.id)
+vswitch_id=$(cat vswitch.id)
+vpc_id=$(cat vpc.id)
+
+# stop instance
+aliyun ecs StopInstance --region ${region} --InstanceId ${instance_id}
+sleep 10
+# aliyun ecs DescribeInstances --RegionId cn-hongkong  | jq -r .Instances.Instance[].Status
+# must be 'Stopped'
+
+# destroy instance
+aliyun ecs DeleteInstance --region ${region} --InstanceId ${instance_id}
+sleep 3
+# delete security group
+aliyun ecs DeleteSecurityGroup --region ${region} --SecurityGroupId ${sec_grp_id}
+sleep 3
+# delete vSwitch
+aliyun vpc DeleteVSwitch --region ${region} --RegionId ${region} --VSwitchId ${vswitch_id}
+sleep 3
+# delete VPC
+aliyun vpc DeleteVpc --region ${region} --RegionId ${region} --VpcId ${vpc_id}
