20a00b38c41a91f5a2066aeee9722482dc9e2474
micki <github@com-science.de>
2025-01-14 16:52:13 +0100

    improve delete ECS alibaba instance script

--- destroy-alibaba-ecs.sh
+++ destroy-alibaba-ecs.sh
@@ -1,25 +1,49 @@
 #!/bin/bash
 
 region=cn-hongkong
-instance_id=$(cat instance.id)
-sec_grp_id=$(cat securitygroup.id)
-vswitch_id=$(cat vswitch.id)
-vpc_id=$(cat vpc.id)
 
 # stop instance
-aliyun ecs StopInstance --region ${region} --InstanceId ${instance_id}
-sleep 30
-# aliyun ecs DescribeInstances --RegionId cn-hongkong  | jq -r .Instances.Instance[].Status
-# must be 'Stopped'
+if [[ -f instance.id ]]; then
+    instance_id=$(cat instance.id)
+    aliyun ecs StopInstance --region ${region} --InstanceId ${instance_id}
+    echo "ECS instance stopped ..."
+    sleep 30
+    # aliyun ecs DescribeInstances --RegionId cn-hongkong  | jq -r .Instances.Instance[].Status
+    # must be 'Stopped'
 
-# destroy instance
-aliyun ecs DeleteInstance --region ${region} --InstanceId ${instance_id}
-sleep 10
+    # destroy instance
+    aliyun ecs DeleteInstance --region ${region} --InstanceId ${instance_id}
+    echo "ECS instance deleted ..."
+    sleep 10
+else
+    echo "No instance ID present"
+fi
+
 # delete security group
-aliyun ecs DeleteSecurityGroup --region ${region} --SecurityGroupId ${sec_grp_id}
-sleep 5
+if [[ -f securitygroup.id ]]; then
+    sec_grp_id=$(cat securitygroup.id)
+    aliyun ecs DeleteSecurityGroup --region ${region} --SecurityGroupId ${sec_grp_id}
+    echo "security group deleted"
+    sleep 5
+else
+    echo "No security group ID present"
+fi
+
 # delete vSwitch
-aliyun vpc DeleteVSwitch --region ${region} --RegionId ${region} --VSwitchId ${vswitch_id}
-sleep 5
+if [[ -f vswitch.id ]]; then
+    vswitch_id=$(cat vswitch.id)
+    aliyun vpc DeleteVSwitch --region ${region} --RegionId ${region} --VSwitchId ${vswitch_id}
+    echo "vSwitch deleted"
+    sleep 5
+else
+    echo "No vSwitch ID present"
+fi
+
 # delete VPC
-aliyun vpc DeleteVpc --region ${region} --RegionId ${region} --VpcId ${vpc_id}
+if [[ -f vpc.id ]]; then
+    vpc_id=$(cat vpc.id)
+    aliyun vpc DeleteVpc --region ${region} --RegionId ${region} --VpcId ${vpc_id}
+    echo "VPC deleted"
+else
+    echo "No VPC ID present"
+fi
